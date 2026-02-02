38eab0289448f2403997bea11ec9ea63bd571d70
samirakurkova <samira_kurkova@rambler.ru>
2026-02-02 18:44:32 +0800

    feat: initial commit - File Synchronization Service with Yandex.Disk support

--- cloud_service.py
+++ cloud_service.py
@@ -1,8 +1,8 @@
 import os
-import requests
-
-from loguru import logger
 from typing import Any, Dict
+
+import requests
+from loguru import logger
 
 
 class CloudService:
@@ -10,22 +10,20 @@ class CloudService:
         self.cloud_folder = cloud_folder
         self.token = token
         self.base_url = base_url
-        self.headers = {'Authorization': f'OAuth {token}',
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'}
+        self.headers = {
+            "Authorization": f"OAuth {token}",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
 
     def check_existence_cloud_folder(self) -> Any:
         """Метод создает папку в директории в облачном хранилище, если ее нет"""
         response = requests.get(
-            self.base_url,
-            headers=self.headers,
-            params={'path': self.cloud_folder}
+            self.base_url, headers=self.headers, params={"path": self.cloud_folder}
         )
         if response.status_code != 200:
             create_response = requests.put(
-                self.base_url,
-                headers=self.headers,
-                params={'path': self.cloud_folder}
+                self.base_url, headers=self.headers, params={"path": self.cloud_folder}
             )
             return create_response
         return response
@@ -33,15 +31,13 @@ class CloudService:
     def get_info(self) -> Dict[str, str]:
         """Метод, получения информации о хранящихся в облачном хранилище файлах"""
         response = requests.get(
-            self.base_url,
-            headers=self.headers,
-            params={'path': self.cloud_folder}
+            self.base_url, headers=self.headers, params={"path": self.cloud_folder}
         )
         cloud_files = {}
         if response.status_code == 200:
-            for item in response.json().get('_embedded').get('items'):
-                if item['type'] == 'file':
-                    cloud_files.update({item['name']: item['modified']})
+            for item in response.json().get("_embedded").get("items"):
+                if item["type"] == "file":
+                    cloud_files.update({item["name"]: item["modified"]})
             return cloud_files
         else:
             return {}
@@ -50,21 +46,22 @@ class CloudService:
         """Метод загрузки файла в облачное хранилище"""
         file_name = os.path.basename(file_path)
         response = requests.get(
-            f'{self.base_url}/upload',
+            f"{self.base_url}/upload",
             headers=self.headers,
-            params={'path': f'{self.cloud_folder}/{file_name}',
-                    'overwrite': True}
+            params={"path": f"{self.cloud_folder}/{file_name}", "overwrite": True},
         )
         if response.status_code == 200:
-            upload_url = response.json().get('href')
+            upload_url = response.json().get("href")
             try:
-                with open(file_path, 'rb') as file:
-                    load_response = requests.put(upload_url, files={'file': file})
+                with open(file_path, "rb") as file:
+                    load_response = requests.put(upload_url, files={"file": file})
             except Exception as exp:
-                logger.error(f'Ошибка при чтении файла {file_name}: {exp}')
+                logger.error(f"Ошибка при чтении файла {file_name}: {exp}")
             return load_response
         else:
-            logger.error(f'Ошибка запроса URL для загрузки файла {file_name}: {response.json().get("message", "error")}')
+            logger.error(
+                f'Ошибка запроса URL для загрузки файла {file_name}: {response.json().get("message")}'
+            )
 
     def reload(self, file_path: str) -> Any:
         """Метод перезаписи файла в облачное хранилище"""
@@ -73,8 +70,8 @@ class CloudService:
     def delete(self, filename: str) -> Any:
         """Метод удаления файла из облачного хранилища"""
         response = requests.delete(
-            f'{self.base_url}',
+            f"{self.base_url}",
             headers=self.headers,
-            params={'path': f'{self.cloud_folder}/{filename}'}
+            params={"path": f"{self.cloud_folder}/{filename}"},
         )
         return response
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import configparser
+import datetime
 import os
 import sys
 import time
-import datetime
-import configparser
 
 from loguru import logger
 
@@ -12,19 +12,25 @@ from cloud_service import CloudService
 def setup_logging(log_file: str) -> None:
     """Настраиваем логгер"""
     logger.remove()
-    log_format = 'FileSynchronizationService:{name}({function}) | {time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}'
+    log_format = "FileSynchronizationService:{name}({function}) | {time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
     logger.add(sys.stderr, format=log_format)
-    logger.add(log_file,
-               format=log_format,
-               rotation="10 MB",
-               retention="1 month",
-               compression="zip")
+    logger.add(
+        log_file,
+        format=log_format,
+        rotation="10 MB",
+        retention="1 month",
+        compression="zip",
+    )
 
 
 def run_cloud_service(local_folder: str, client: CloudService) -> None:
     """Запустить облачный сервис"""
-    local_files = {file_name: datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(local_folder, file_name)))
-                   for file_name in os.listdir(local_folder)}
+    local_files = {
+        file_name: datetime.datetime.fromtimestamp(
+            os.path.getmtime(os.path.join(local_folder, file_name))
+        )
+        for file_name in os.listdir(local_folder)
+    }
     cloud_files = client.get_info()
 
     # Получаем только локальные файлы, которых нет в облачном хранилище
@@ -41,89 +47,119 @@ def run_cloud_service(local_folder: str, client: CloudService) -> None:
         for file_name in only_local_files:
             load = client.load(file_path=os.path.join(local_folder, file_name))
             if load.status_code == 201:
-                logger.info(f'Файл {file_name} успешно записан в облачное хранилище')
+                logger.info(f"Файл {file_name} успешно записан в облачное хранилище")
             else:
-                logger.error(f'Файл {file_name} не записан: {load.json().get("message", "error")}')
+                logger.error(
+                    f'Файл {file_name} не записан: {load.json().get("message", "error")}'
+                )
 
     # Удаляем файлы из облачного хранилища
     if only_local_files:
         for file_name in only_cloud_files:
             delete = client.delete(filename=file_name)
             if delete.status_code == 204:
-                logger.info(f'Файл {file_name} успешно удален из облачного хранилища')
+                logger.info(f"Файл {file_name} успешно удален из облачного хранилища")
             else:
-                logger.error(f'Ошибка удаления файла {file_name} из облачного хранилища: '
-                             f'{delete.json().get("message", "error")}')
+                logger.error(
+                    f"Ошибка удаления файла {file_name} из облачного хранилища: "
+                    f'{delete.json().get("message", "error")}'
+                )
 
     # Сравниваем времена общих файлов
     if common_files:
         for file_name in common_files:
-            if local_files[file_name].timestamp() > (datetime.datetime.fromisoformat(
-                    cloud_files[file_name].replace('Z', '+00:00'))).timestamp():
+            if (
+                local_files[file_name].timestamp()
+                > (
+                    datetime.datetime.fromisoformat(
+                        cloud_files[file_name].replace("Z", "+00:00")
+                    )
+                ).timestamp()
+            ):
                 reload = client.reload(file_path=os.path.join(local_folder, file_name))
                 if reload.status_code == 201:
-                    logger.info(f'Файл {file_name} успешно перезаписан в облачном хранилище')
+                    logger.info(
+                        f"Файл {file_name} успешно перезаписан в облачном хранилище"
+                    )
                 else:
-                    logger.error(f'Файл {file_name} не перезаписан: {reload.json().get("message", "error")}')
+                    logger.error(
+                        f'Файл {file_name} не перезаписан: {reload.json().get("message", "error")}'
+                    )
 
 
 def main():
     config = configparser.ConfigParser()
-    config.read('config.ini', encoding='UTF-8')
+    config.read("config.ini", encoding="UTF-8")
 
-    LOCAL_FOLDER_PATH = config['DEFAULT']['LOCAL_FOLDER_PATH']
-    LOCAL_FOLDER = config['DEFAULT']['LOCAL_FOLDER_PATH']
-    SYNC_INTERVAL = int(config['DEFAULT']['SYNC_INTERVAL'])
-    LOG_FILE = config['DEFAULT']['LOG_FILE']
-    CLOUD_FOLDER = config['YandexDisk']['CLOUD_FOLDER']
-    YANDEX_TOKEN = config['YandexDisk']['YANDEX_TOKEN']
-    BASE_URL = config['YandexDisk']['BASE_URL']
+    LOCAL_FOLDER_PATH = config["DEFAULT"]["LOCAL_FOLDER_PATH"]
+    LOCAL_FOLDER = config["DEFAULT"]["LOCAL_FOLDER_PATH"]
+    SYNC_INTERVAL = int(config["DEFAULT"]["SYNC_INTERVAL"])
+    LOG_FILE = config["DEFAULT"]["LOG_FILE"]
+    CLOUD_FOLDER = config["YandexDisk"]["CLOUD_FOLDER"]
+    YANDEX_TOKEN = config["YandexDisk"]["YANDEX_TOKEN"]
+    BASE_URL = config["YandexDisk"]["BASE_URL"]
 
     setup_logging(LOG_FILE)
-    logger.info(f'Программа синхронизации файлов начинает работу с директорией {LOCAL_FOLDER_PATH}')
+    logger.info(
+        f"Программа синхронизации файлов начинает работу с директорией {LOCAL_FOLDER_PATH}"
+    )
 
     if not os.path.isdir(LOCAL_FOLDER_PATH):
-        logger.error(f'Локальная папка {LOCAL_FOLDER} не существует')
+        logger.error(f"Локальная папка {LOCAL_FOLDER} не существует")
         sys.exit(1)
 
     try:
-        logger.info(f'Инициализация сервиса синхронизации файлов')
-        client = CloudService(cloud_folder=CLOUD_FOLDER,
-                              token=YANDEX_TOKEN,
-                              base_url=BASE_URL)
+        logger.info("Инициализация сервиса синхронизации файлов")
+        client = CloudService(
+            cloud_folder=CLOUD_FOLDER, token=YANDEX_TOKEN, base_url=BASE_URL
+        )
         response = client.check_existence_cloud_folder()
         if response.status_code == 200:
-            logger.info(f'Папка {CLOUD_FOLDER} в облачном хранилище существует')
+            logger.info(f"Папка {CLOUD_FOLDER} в облачном хранилище существует")
         elif response.status_code == 201:
-            logger.info(f'Папка {CLOUD_FOLDER} в облачном хранилище успешно создана')
+            logger.info(f"Папка {CLOUD_FOLDER} в облачном хранилище успешно создана")
         else:
-            logger.error(f'Ошибка создания папки {CLOUD_FOLDER} в облачном хранилище: '
-                         f'{response.json().get("message")}')
+            logger.error(
+                f"Ошибка создания папки {CLOUD_FOLDER} в облачном хранилище: "
+                f'{response.json().get("message")}'
+            )
     except Exception as exp:
-        logger.error(f'Ошибка подключения к сервису синхронизации файлов локальной папки {LOCAL_FOLDER}: {exp}')
+        logger.error(
+            f"Ошибка подключения к сервису синхронизации файлов локальной папки {LOCAL_FOLDER}: {exp}"
+        )
         sys.exit(1)
 
-    logger.info(f'Первая синхронизация файлов локальной папки {LOCAL_FOLDER}')
+    logger.info(f"Первая синхронизация файлов локальной папки {LOCAL_FOLDER}")
     if not os.path.exists(LOCAL_FOLDER_PATH):
-        logger.error(f'Локальная папка {LOCAL_FOLDER} для синхронизации в директории '
-                     f'{LOCAL_FOLDER_PATH} отсутствует')
+        logger.error(
+            f"Локальная папка {LOCAL_FOLDER} для синхронизации в директории "
+            f"{LOCAL_FOLDER_PATH} отсутствует"
+        )
         sys.exit(1)
 
     try:
         run_cloud_service(LOCAL_FOLDER_PATH, client)
     except Exception as exp:
-        logger.error(f'Ошибка первой синхронизации файлов локальной папки {LOCAL_FOLDER}: {exp}')
+        logger.error(
+            f"Ошибка первой синхронизации файлов локальной папки {LOCAL_FOLDER}: {exp}"
+        )
 
-    logger.info(f'Цикл периодической синхронизации файлов локальной папки {LOCAL_FOLDER} запущен')
+    logger.info(
+        f"Цикл периодической синхронизации файлов локальной папки {LOCAL_FOLDER} запущен"
+    )
     while True:
         try:
             time.sleep(SYNC_INTERVAL)
             run_cloud_service(LOCAL_FOLDER_PATH, client)
         except KeyboardInterrupt:
-            logger.error(f'Синхронизация файлов локальной папки {LOCAL_FOLDER} прервана пользователем')
+            logger.error(
+                f"Синхронизация файлов локальной папки {LOCAL_FOLDER} прервана пользователем"
+            )
         except Exception as exp:
-            logger.error(f'Ошибка синхронизации файлов локальной папки {LOCAL_FOLDER}: {exp}')
+            logger.error(
+                f"Ошибка синхронизации файлов локальной папки {LOCAL_FOLDER}: {exp}"
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
